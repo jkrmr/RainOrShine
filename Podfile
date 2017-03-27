179f76a2531c8886dda118849f055066b6708238
Jake Romer <jkrmr.io@gmail.com>
2017-03-27 07:20:07 -0700

    Add cocoapods, base UI

---
+++ Podfile
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+source "https://github.com/CocoaPods/Specs.git"
+
+platform :ios, "10.0"
+use_frameworks!
+
+target "RainOrShine" do
+  # pod "SWTableViewCell", "~> 0.3.7"
+  pod "Alamofire", "~> 4.4"
+end
---
+++ RainOrShine/WeatherVC.swift
@@ -0,0 +1,41 @@
+//
+//  WeatherVC.swift
+//  RainOrShine
+//
+//  Created by Jake Romer on 3/26/17.
+//  Copyright © 2017 Jake Romer. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var temperatureLabel: UILabel!
+  @IBOutlet weak var locationLabel: UILabel!
+  @IBOutlet weak var weatherImage: UIImageView!
+  @IBOutlet weak var weatherDescription: UILabel!
+  @IBOutlet weak var tableView: UITableView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    tableView.delegate = self
+    tableView.dataSource = self
+  }
+
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 6
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+    return cell
+  }
+
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+  }
+}
+
