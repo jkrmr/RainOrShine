8dfc2b5351c99a7ff57e6ca6cf27c17f44cf065b
Jake Romer <jkrmr.io@gmail.com>
2017-03-26 20:42:27 -0700

    Initial Commit

---
+++ RainOrShine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RainOrShine
+//
+//  Created by Jake Romer on 3/26/17.
+//  Copyright © 2017 Jake Romer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
